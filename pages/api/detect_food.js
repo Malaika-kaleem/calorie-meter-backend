5b5989fc6a8959eaac931ec240ea6d6659ae1b9f
Malaika-kaleem <165413539+Malaika-kaleem@users.noreply.github.com>
2025-07-19 12:19:00 +0500

    Update detect_food.js

--- pages/api/detect_food.js
+++ pages/api/detect_food.js
@@ -6,7 +6,7 @@ export const config = {
     },
 }
 async function detectFoodAndCalories(base64Image) {
-    const apiKey = process.env.NEXT_PUBLIC_GOOGLE_AI_API_KEY;
+    const apiKey = process.env.GOOGLE_AI_API_KEY;
     const model = 'gemini-1.5-flash';
     const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
 
